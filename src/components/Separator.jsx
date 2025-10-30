27e41a5959bddabc0763193cbd2b44d3181a5e44
thecodejit <43138013+thecodejit@users.noreply.github.com>
2025-10-30 23:19:11 +0530

    Add separator component

---
+++ src/components/Separator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Separator = () => {
+  return (
+    <div className="border border-gray-200">
+      <div className=" h-7 w-[94%] sm:w-3xl mx-auto  bg-[linear-gradient(to_right,#80808012_1px,transparent_4px),linear-gradient(to_bottom,#80808012_1px,transparent_4px)] bg-size-[14px_14px] border-x border-gray-200"></div>
+    </div>
+  );
+};
+
+export default Separator;
